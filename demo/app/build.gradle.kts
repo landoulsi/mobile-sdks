67f02a70a4ecd52ae2611b210dd991cb30bd05a9
Ahmed <landoulsi.ahmed@gmail.com>
2026-08-27 19:33:45 +0400

    Rename styles module to design and update references

--- demo/app/build.gradle.kts
+++ demo/app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(project(":design"))
 
-    implementation(project(":styles"))
     implementation(project(":payment:shared"))
     implementation(project(":update:shared"))
 }
--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.landoulsi.styles"
+    namespace = "com.landoulsi.design"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
